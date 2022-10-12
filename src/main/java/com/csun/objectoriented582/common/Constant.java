f321457bccbafeb07c8f96c87cb7599b1cd25073
luhao1861 <90377231+luhao1861@users.noreply.github.com>
2022-10-11 21:28:24 -0700

    Initial commit

---
+++ src/main/java/com/csun/objectoriented582/common/Constant.java
@@ -0,0 +1,8 @@
+package com.csun.objectoriented582.common;
+
+public class Constant {
+    public final static Long STATUS_ON = 1L;
+    public final static Long STATUS_OFF = 0L;
+    public static final String DEFULT_PASSWORD = "123456";
+    public static final String DEFULT_AVATAR = "https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png";
+}
